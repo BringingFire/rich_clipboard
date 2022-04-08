c36e99701abb9022995ab4d5be6f821133cf1a8f
Joshua Matthews <josh@jmatth.com>
2022-04-08 16:55:29 -0400

    Adding docstrings

--- lib/rich_clipboard.dart
+++ lib/rich_clipboard.dart
@@ -8,6 +8,7 @@ const _kMimeTextHtml = 'text/html';
 
 final platformSupported = !kIsWeb && Platform.isMacOS;
 
+/// Data from the system clipboard.
 class RichClipboardData implements ClipboardData {
   RichClipboardData({this.text, this.html});
   RichClipboardData.fromMap(Map<String, String?> map)
@@ -24,14 +25,23 @@ class RichClipboardData implements ClipboardData {
         _kMimeTextPlain: text,
         _kMimeTextHtml: html,
       };
+
   @override
-  String toString() =>
-      'RichClipboardData{ plainText: $text, htmlText: $html }';
+  String toString() => 'RichClipboardData{ plainText: $text, htmlText: $html }';
 }
 
+/// Utility methods for interacting with the systems clipboard with support for
+/// various data formats.
 class RichClipboard {
+  // Prevent instantiation or extension
+  RichClipboard._();
+
   static const MethodChannel _channel = MethodChannel('rich_clipboard');
 
+  /// Retrieves data from the clipboard.
+  ///
+  /// Returns a future which completes to a [RichClipboardData] containing all
+  /// available formats that were found in the clipboard.
   static Future<RichClipboardData> getData() async {
     if (!platformSupported) {
       final data = await Clipboard.getData(Clipboard.kTextPlain);
@@ -47,6 +57,7 @@ class RichClipboard {
     return RichClipboardData.fromMap(data);
   }
 
+  /// Clears the system clipboard and then stores the provided data.
   static Future<void> setData(RichClipboardData data) async {
     if (!platformSupported) {
       await Clipboard.setData(ClipboardData(text: data.text));
@@ -61,6 +72,12 @@ class RichClipboard {
     return await _channel.invokeMethod('RichClipboard.getItemCount');
   }
 
+  /// Returns a list of strings representing the data types available in the
+  /// system clipboard.
+  ///
+  /// Primarily useful for debugging. The returned strings are platform
+  /// dependent and likely do not conform to anything easily usable like MIME
+  /// types.
   static Future<List<String>> getAvailableTypes() async {
     if (!platformSupported) {
       return [];
