7aee08b970b0bcb3793d08891b5929b28a785106
Joshua Matthews <josh@jmatth.com>
2022-04-18 16:51:23 -0400

    Improving getAvailableTypes on windows by converting UINTs to strings

--- windows/rich_clipboard_plugin.cpp
+++ windows/rich_clipboard_plugin.cpp
@@ -16,6 +16,69 @@
 
 namespace {
 
+  std::optional<std::string> CfToString(UINT cf_format)
+  {
+    switch (cf_format)
+    {
+    case CF_TEXT:
+      return "CF_TEXT";
+    case CF_BITMAP:
+      return "CF_BITMAP";
+    case CF_METAFILEPICT:
+      return "CF_METAFILEPICT";
+    case CF_SYLK:
+      return "CF_SYLK";
+    case CF_DIF:
+      return "CF_DIF";
+    case CF_TIFF:
+      return "CF_TIFF";
+    case CF_OEMTEXT:
+      return "CF_OEMTEXT";
+    case CF_DIB:
+      return "CF_DIB";
+    case CF_PALETTE:
+      return "CF_PALETTE";
+    case CF_PENDATA:
+      return "CF_PENDATA";
+    case CF_RIFF:
+      return "CF_RIFF";
+    case CF_WAVE:
+      return "CF_WAVE";
+    case CF_UNICODETEXT:
+      return "CF_UNICODETEXT";
+    case CF_ENHMETAFILE:
+      return "CF_ENHMETAFILE";
+    case CF_HDROP:
+      return "CF_HDROP";
+    case CF_LOCALE:
+      return "CF_LOCALE";
+    case CF_DIBV5:
+      return "CF_DIBV5";
+    case CF_MAX:
+      return "CF_MAX";
+    case CF_OWNERDISPLAY:
+      return "CF_OWNERDISPLAY";
+    case CF_DSPTEXT:
+      return "CF_DSPTEXT";
+    case CF_DSPBITMAP:
+      return "CF_DSPBITMAP";
+    case CF_DSPMETAFILEPICT:
+      return "CF_DSPMETAFILEPICT";
+    case CF_DSPENHMETAFILE:
+      return "CF_DSPENHMETAFILE";
+    case CF_PRIVATEFIRST:
+      return "CF_PRIVATEFIRST";
+    case CF_PRIVATELAST:
+      return "CF_PRIVATELAST";
+    case CF_GDIOBJFIRST:
+      return "CF_GDIOBJFIRST";
+    case CF_GDIOBJLAST:
+      return "CF_GDIOBJLAST";
+    }
+
+    return std::nullopt;
+  }
+
 class RichClipboardPlugin : public flutter::Plugin {
  public:
   static void RegisterWithRegistrar(flutter::PluginRegistrarWindows *registrar);
@@ -55,21 +118,8 @@ RichClipboardPlugin::~RichClipboardPlugin() {}
 
 void RichClipboardPlugin::HandleMethodCall(
     const flutter::MethodCall<flutter::EncodableValue> &method_call,
-    std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result) {
-  if (method_call.method_name().compare("getPlatformVersion") == 0) {
-    std::ostringstream version_stream;
-    version_stream << "Windows ";
-    if (IsWindows10OrGreater()) {
-      version_stream << "10+";
-    } else if (IsWindows8OrGreater()) {
-      version_stream << "8";
-    } else if (IsWindows7OrGreater()) {
-      version_stream << "7";
-    }
-    result->Success(flutter::EncodableValue(version_stream.str()));
-    return;
-  }
-
+    std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result)
+{
   if (method_call.method_name().compare("getAvailableTypes") == 0)
   {
     if (!OpenClipboard(NULL))
@@ -78,16 +128,23 @@ void RichClipboardPlugin::HandleMethodCall(
       return;
     }
 
-    flutter::EncodableList availableTypes;
-    auto nextType = EnumClipboardFormats(NULL);
-    while (nextType != NULL)
+    flutter::EncodableList available_types;
+    auto next_type = EnumClipboardFormats(NULL);
+    while (next_type != NULL)
     {
-      availableTypes.push_back(flutter::EncodableValue(std::to_string(nextType)));
-      nextType = EnumClipboardFormats(nextType);
+      if (auto next_type_string = CfToString(next_type); next_type_string)
+      {
+        available_types.push_back(flutter::EncodableValue(*next_type_string));
+      }
+      else
+      {
+        available_types.push_back(flutter::EncodableValue(std::to_string(next_type)));
+      }
+      next_type = EnumClipboardFormats(next_type);
     }
 
     CloseClipboard();
-    result->Success(availableTypes);
+    result->Success(available_types);
     return;
   }
 
