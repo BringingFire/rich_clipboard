3268bd6636399b8f91a269386be7c15634699c2c
Joshua Matthews <josh@jmatth.com>
2022-05-09 09:51:38 -0400

    WIP: adding incomplete Linux implementation with only getAvaliableTypes implemented

---
+++ rich_clipboard_linux/linux/rich_clipboard_linux_plugin.cc
@@ -0,0 +1,102 @@
+#include "include/rich_clipboard_linux/rich_clipboard_plugin.h"
+
+#include <flutter_linux/flutter_linux.h>
+#include <gtk/gtk.h>
+#include <sys/utsname.h>
+
+#include <cstring>
+
+const char kChannelName[] = "com.bringingfire.rich_clipboard";
+// const char kGetData[] = "getData";
+// const char kSetData[] = "setData";
+const char kGetAvailableTypes[] = "getAvailableTypes";
+
+struct _FlRichClipboardPlugin
+{
+  GObject parent_instance;
+
+  FlPluginRegistrar *registrar;
+
+  // Connection to Flutter engine.
+  FlMethodChannel *channel;
+};
+
+G_DEFINE_TYPE(FlRichClipboardPlugin, fl_rich_clipboard_plugin, g_object_get_type())
+
+static void gtk_clipboard_request_targets_callback(
+    GtkClipboard *clipboard,
+    GdkAtom *atoms,
+    gint n_atoms,
+    gpointer user_data)
+{
+  g_autoptr(FlMethodCall) method_call = static_cast<FlMethodCall *>(user_data);
+
+  g_autoptr(FlValue) result = fl_value_new_list();
+  for (gint i = 0; i < n_atoms; i++)
+  {
+    auto target = gdk_atom_name(atoms[i]);
+    fl_value_append_take(result, fl_value_new_string(target));
+    g_free(target);
+  }
+  fl_method_call_respond_success(method_call, result, nullptr);
+}
+
+// Called when a method call is received from Flutter.
+static void method_call_cb(FlMethodChannel *channel, FlMethodCall *method_call,
+                           gpointer user_data)
+{
+  const gchar *method = fl_method_call_get_name(method_call);
+
+  g_autoptr(FlMethodResponse) response = nullptr;
+  if (strcmp(method, kGetAvailableTypes) == 0)
+  {
+    auto *clipboard = gtk_clipboard_get_default(gdk_display_get_default());
+    gtk_clipboard_request_targets(
+        clipboard,
+        gtk_clipboard_request_targets_callback,
+        g_object_ref(method_call));
+    // response = FL_METHOD_RESPONSE(fl_method_success_response_new(result));
+  }
+  else
+  {
+    response = FL_METHOD_RESPONSE(fl_method_not_implemented_response_new());
+    g_autoptr(GError) error = nullptr;
+    if (!fl_method_call_respond(method_call, response, &error))
+      g_warning("Failed to send method call response: %s", error->message);
+  }
+}
+
+static void fl_rich_clipboard_plugin_dispose(GObject *object)
+{
+  G_OBJECT_CLASS(fl_rich_clipboard_plugin_parent_class)->dispose(object);
+}
+
+static void fl_rich_clipboard_plugin_class_init(FlRichClipboardPluginClass *klass)
+{
+  G_OBJECT_CLASS(klass)->dispose = fl_rich_clipboard_plugin_dispose;
+}
+
+FlRichClipboardPlugin *fl_rich_clipboard_plugin_new(FlPluginRegistrar *registrar)
+{
+  FlRichClipboardPlugin *self = FL_MY_PLUGIN_PLUGIN(
+      g_object_new(fl_rich_clipboard_plugin_get_type(), nullptr));
+
+  self->registrar = FL_PLUGIN_REGISTRAR(g_object_ref(registrar));
+
+  g_autoptr(FlStandardMethodCodec) codec = fl_standard_method_codec_new();
+  self->channel =
+      fl_method_channel_new(fl_plugin_registrar_get_messenger(registrar),
+                            kChannelName, FL_METHOD_CODEC(codec));
+  fl_method_channel_set_method_call_handler(self->channel, method_call_cb,
+                                            g_object_ref(self), g_object_unref);
+
+  return self;
+}
+
+static void fl_rich_clipboard_plugin_init(FlRichClipboardPlugin *self) {}
+
+void rich_clipboard_plugin_register_with_registrar(FlPluginRegistrar *registrar)
+{
+  FlRichClipboardPlugin *plugin = fl_rich_clipboard_plugin_new(registrar);
+  g_object_unref(plugin);
+}
