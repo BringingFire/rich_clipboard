f7aab683a0c0e2560749cef4fefeb02561d2a24f
Joshua Matthews <josh@jmatth.com>
2022-05-13 15:23:15 -0400

    WIP: Implementing setData for Linux

--- rich_clipboard_linux/linux/rich_clipboard_linux_plugin.cc
+++ rich_clipboard_linux/linux/rich_clipboard_linux_plugin.cc
@@ -4,14 +4,49 @@
 #include <gtk/gtk.h>
 #include <sys/utsname.h>
 
+#include <memory>
+#include <string>
 #include <cstring>
+
+using namespace std;
 
 const char kChannelName[] = "com.bringingfire.rich_clipboard";
 const char kGetData[] = "getData";
-// const char kSetData[] = "setData";
+const char kSetData[] = "setData";
 const char kGetAvailableTypes[] = "getAvailableTypes";
 const char kMimeTextPlain[] = "text/plain";
 const char kMimeTextHtml[] = "text/html";
+
+const GdkAtom kGdkAtomTextPlain = gdk_atom_intern_static_string(kMimeTextPlain);
+const GdkAtom kGdkAtomTextHtml = gdk_atom_intern_static_string(kMimeTextHtml);
+
+const guint kUserInfoTextPlain = 1;
+const guint kUserInfoTextHtml = 2;
+
+class RichClipboardData
+{
+private:
+  unique_ptr<string> textPlain;
+  unique_ptr<string> textHtml;
+
+public:
+  string *getTextPlain()
+  {
+    return textPlain.get();
+  }
+  void setTextPlain(const gchar *text)
+  {
+    textPlain.reset(new string(text));
+  }
+  string *getTextHtml()
+  {
+    return textHtml.get();
+  }
+  void setTextHtml(const gchar *text)
+  {
+    textHtml.reset(new string(text));
+  }
+};
 
 struct _FlRichClipboardPlugin
 {
@@ -43,6 +78,31 @@ static void gtk_clipboard_request_targets_callback(
   fl_method_call_respond_success(method_call, result, nullptr);
 }
 
+static void gtk_clipboard_get_target_text_callback(
+    GtkClipboard *clipboard,
+    GtkSelectionData *selectionData,
+    guint info,
+    gpointer user_data_or_owner)
+{
+  auto *clipboardData = reinterpret_cast<RichClipboardData *>(user_data_or_owner);
+  if (info == kUserInfoTextPlain && clipboardData->getTextPlain() != nullptr)
+  {
+    auto *textPlainCString = clipboardData->getTextPlain()->c_str();
+    gtk_selection_data_set_text(selectionData, textPlainCString, strlen(textPlainCString));
+  }
+  else if (info == kUserInfoTextHtml && clipboardData->getTextHtml() != nullptr)
+  {
+    auto *textHtmlCString = clipboardData->getTextHtml()->c_str();
+    gtk_selection_data_set(selectionData, kGdkAtomTextHtml, 8, (guchar *)textHtmlCString, strlen(textHtmlCString));
+  }
+}
+
+static void gtk_clipboard_clear_text_callback(GtkClipboard *clipboard, gpointer user_data_or_owner)
+{
+  auto *clipboardData = reinterpret_cast<RichClipboardData *>(user_data_or_owner);
+  delete clipboardData;
+}
+
 // Called when a method call is received from Flutter.
 static void method_call_cb(FlMethodChannel *channel, FlMethodCall *method_call,
                            gpointer user_data)
@@ -70,13 +130,13 @@ static void method_call_cb(FlMethodChannel *channel, FlMethodCall *method_call,
       g_free(text);
     }
 
-    auto *htmlData = gtk_clipboard_wait_for_contents(clipboard, gdk_atom_intern_static_string(kMimeTextHtml));
+    auto *htmlData = gtk_clipboard_wait_for_contents(clipboard, kGdkAtomTextHtml);
     if (htmlData != nullptr)
     {
       // Testing shows that GTK will just return plain if no HTML data is available, so make sure we actually
       // have HTML before adding it to the result map.
       auto htmlType = gtk_selection_data_get_data_type(htmlData);
-      auto htmlTypeName = gdk_atom_name(htmlType);
+      auto *htmlTypeName = gdk_atom_name(htmlType);
       if (strcmp(htmlTypeName, kMimeTextHtml) == 0)
       {
         gint htmlLen;
@@ -91,6 +151,48 @@ static void method_call_cb(FlMethodChannel *channel, FlMethodCall *method_call,
     }
 
     fl_method_call_respond_success(method_call, result, nullptr);
+  }
+  else if (strcmp(method, kSetData) == 0)
+  {
+    auto *clipboard = gtk_clipboard_get_default(gdk_display_get_default());
+    gtk_clipboard_clear(clipboard);
+
+    auto *args = fl_method_call_get_args(method_call);
+
+    auto *clipboardData = new RichClipboardData();
+    auto *textPlainValue = fl_value_lookup_string(args, kMimeTextPlain);
+    if (textPlainValue != nullptr)
+    {
+      auto *textPlainCString = fl_value_get_string(textPlainValue);
+      clipboardData->setTextPlain(textPlainCString);
+    }
+    auto *textHtmlValue = fl_value_lookup_string(args, kMimeTextHtml);
+    if (textHtmlValue != nullptr)
+    {
+      auto *textHtmlCString = fl_value_get_string(textHtmlValue);
+      clipboardData->setTextHtml(textHtmlCString);
+    }
+
+    auto *targetList = gtk_target_list_new(nullptr, 0);
+    gtk_target_list_add_text_targets(targetList, kUserInfoTextPlain);
+    gtk_target_list_add(targetList, kGdkAtomTextHtml, 0, kUserInfoTextHtml);
+
+    gint numTargets = 1;
+    auto *targetTable = gtk_target_table_new_from_list(targetList, &numTargets);
+    gtk_clipboard_set_with_data(
+        clipboard,
+        targetTable,
+        numTargets,
+        gtk_clipboard_get_target_text_callback,
+        gtk_clipboard_clear_text_callback,
+        clipboardData);
+    gtk_clipboard_set_can_store(clipboard, targetTable, numTargets);
+    gtk_clipboard_store(clipboard);
+
+    gtk_target_list_unref(targetList);
+    gtk_target_table_free(targetTable, numTargets);
+
+    fl_method_call_respond_success(method_call, nullptr, nullptr);
   }
   else
   {
