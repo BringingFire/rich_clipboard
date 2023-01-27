2aad0adf18c491feeb9ef8fdbe6572fd70204bb2
Joshua Matthews <josh@jmatth.com>
2023-01-26 20:22:14 -0500

    Updating macOS runner code in example with new generated code from 3.7.0

--- rich_clipboard/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ rich_clipboard/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,7 +7,7 @@ import Foundation
 
 import audio_session
 import just_audio
-import path_provider_macos
+import path_provider_foundation
 import rich_clipboard_macos
 import sqflite
 import url_launcher_macos
