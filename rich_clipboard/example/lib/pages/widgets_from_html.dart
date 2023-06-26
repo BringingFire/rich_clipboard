54d24918617ef8db5ec39a394556542899727e57
Joshua Matthews <josh@jmatth.com>
2023-06-26 14:49:01 -0400

    Updating example app to work with Flutter 3.10

--- rich_clipboard/example/lib/pages/widgets_from_html.dart
+++ rich_clipboard/example/lib/pages/widgets_from_html.dart
@@ -22,7 +22,6 @@ class _WidgetsFromHtmlPageState extends State<WidgetsFromHtmlPage> {
             child: SingleChildScrollView(
               child: HtmlWidget(
                 _html,
-                isSelectable: true,
               ),
             ),
           ),
